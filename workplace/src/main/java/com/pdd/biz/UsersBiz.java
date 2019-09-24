1173490f25d8f76cc7778a8a7872a5c4fc47c449
Administrator <Administrator@SKY-20190903TAC>
2019-09-24 10:22:41 +0800

    whl users

---
+++ workplace/src/main/java/com/pdd/biz/UsersBiz.java
@@ -0,0 +1,9 @@
+package com.pdd.biz;
+
+import com.pdd.entity.Users;
+
+public interface UsersBiz {
+  Users checkLogin(String username ,String password);
+  void regedit(Users u);
+  
+}
---
+++ workplace/src/main/java/com/pdd/controller/UsersController.java
@@ -0,0 +1,42 @@
+package com.pdd.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pdd.biz.UsersBiz;
+import com.pdd.entity.Users;
+
+@RestController
+@RequestMapping("/api")
+public class UsersController {
+
+	@Autowired
+private	UsersBiz ub;
+	
+	
+	public UsersBiz getUb() {
+		return ub;
+	}
+
+
+	public void setUb(UsersBiz ub) {
+		this.ub = ub;
+	}
+
+
+	@RequestMapping("/login")
+	public  Users login(@RequestBody String username,String password ) {
+		Users u = ub.checkLogin(username, password);
+		if(u!=null) {
+			return u;
+			
+		}else {
+			return null;
+		}
+		
+		
+		
+	}
+}
