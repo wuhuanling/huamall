b40d6f210adc35706575d6ee4670ba2b6dc7ff5a
guoyz-Xiaomi <findtv@126.com>
2019-09-27 10:59:08 +0800

    引入Redux数据引擎,index/index页面更新

--- FontEndDev/huamall/src/Pages/Index/index.js
+++ FontEndDev/huamall/src/Pages/Index/index.js
@@ -1,19 +1,93 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import 'antd/dist/antd.css'
+import { Carousel,Menu, Dropdown, Button } from 'antd'
+import store from '../../store'
+
+const menu = (
+    <Menu>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+          1st menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+          2nd menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+          3rd menu item
+        </a>
+      </Menu.Item>
+    </Menu>
+);
 
 class index extends Component {
+    constructor(props) {
+        super(props)
+        this.setState = store.getState()
+        this.storeChange = this.storeChange.bind(this)
+        store.subscribe(this.storeChange)
+
+        //绑定this
+
+    }
     render() {
         return (
-            <div>
-                <h2>Hello world</h2>
-
+            <div className="indexPage">
+                <aside>
+                    <Dropdown overlay={menu} placement="topCenter">
+                        <Button>topCenter</Button>
+                    </Dropdown>
+                    <Dropdown overlay={menu} placement="topCenter">
+                        <Button>topCenter</Button>
+                    </Dropdown>
+                    <Dropdown overlay={menu} placement="topCenter">
+                        <Button>topCenter</Button>
+                    </Dropdown>
+                    <Dropdown overlay={menu} placement="topCenter">
+                        <Button>topCenter</Button>
+                    </Dropdown>
+                    <Dropdown overlay={menu} placement="topCenter">
+                        <Button>topCenter</Button>
+                    </Dropdown>
+                    <Dropdown overlay={menu} placement="topCenter">
+                        <Button>topCenter</Button>
+                    </Dropdown>
+                </aside>
+                <main>
+                <Carousel autoplay>
+                    <div>
+                    <h3>1</h3>
+                    </div>
+                    <div>
+                    <h3>2</h3>
+                    </div>
+                    <div>
+                    <h3>3</h3>
+                    </div>
+                    <div>
+                    <h3>4</h3>
+                    </div>
+                </Carousel>
+                </main>
             </div>
-        );
+        )
+    }
+    //生命周期函数
+    componentDidMount() {
+        console.log("挂载完成")
+    }
+    //最定义方法
+    storeChange(){
+        this.setState(store.getState()) //刷新一下stat
     }
 }
 
-index.propTypes = {
+// index.propTypes = {
 
-};
+// };
 
 export default index;
--- FontEndDev/huamall/src/store/reducer.js
+++ FontEndDev/huamall/src/store/reducer.js
@@ -1,32 +1,20 @@
 //默认数据
 const defaultState = {
-    inputValue: '输入的值',
+    inputValue: '',
     list:[
-        '早上4点起床',
-        '中午下班游泳一小时'
     ]
 }
 export default (state = defaultState,action)=>{  //就是一个方法函数
-    // console.log(state,action)
-    if(action.type === 'change_input_value'){
-        let newState = JSON.parse(JSON.stringify(state))
-        newState.inputValue = action.value
-        return newState
-    }
 
-    //state值只能传递，不能使用
-    if(action.type === 'addItem' ){ //根据type值，编写业务逻辑
-        let newState = JSON.parse(JSON.stringify(state))
-        newState.list.push(newState.inputValue)  //push新的内容到列表中去
-        newState.inputValue = ''
-        return newState
-    }
 
-    if(action.type === 'deleteItem' ){
-        let newState = JSON.parse(JSON.stringify(state))
-        newState.list.splice(action.index,1)  //删除数组中对应的值
-        return newState
-    }
+    // //state值只能传递，不能使用
+    // if(action.type === 'addItem' ){ //根据type值，编写业务逻辑
+    //     let newState = JSON.parse(JSON.stringify(state))
+    //     newState.list.push(newState.inputValue)  //push新的内容到列表中去
+    //     newState.inputValue = ''
+    //     return newState
+    // }
+
 
     return state
 }
